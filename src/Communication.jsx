559641e81f8f54bc209617eab22a1e26c7fe8f55
chandru143-rv <vchandrukadambur71@gmail.com>
2025-03-27 23:33:15 +0530

    change

--- src/Communication.jsx
+++ src/Communication.jsx
@@ -11,14 +11,14 @@ const Communication = () => {
            <div className="topic">
             <p className='title h3'>contact me</p>
             </div>
-        < input type="hidden" name='access_key' value={"8c5aba53-566d-45e5-a380-b805b5ba513e"} />
+        < input type="hidden" name='access_key' value={"73c1bc246fe29ea7f4e20d9d69931ace"} />
              <input type='text' placeholder='Username' name='name' className='user' required />
              <input type='email' placeholder='Gmail' name='email'  className='Gmail' required/>
              <textarea name='message' placeholder='write on your message' required/>
              <input type="hidden" name="_captcha" value="false"></input>
              <input type="hidden" name="_next" value="https://yourdomain.co/thanks.html"></input>
              <div className="topic">
-              <button type='sumbit' className="px-1">Sumbmit form<i class="bi bi-arrow-right"></i> </button>
+              <button type='sumbit' className="px-1">Submit form<i class="bi bi-arrow-right"></i> </button>
               </div> 
         </div>      
         </form>
