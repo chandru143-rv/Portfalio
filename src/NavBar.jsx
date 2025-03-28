aa72927910163331134deb25370dd2291e769e6d
chandru143-rv <vchandrukadambur71@gmail.com>
2025-03-28 18:55:35 +0530

    modify

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -3,48 +3,79 @@ import { Link,Outlet} from 'react-router-dom'
 
 const NavBar = () => {
   return (
+    
+
+
     <>
-       <nav class="navbar navbar-expand-lg">
-              <div class="container-fluid ">
-                       <Link to="/" class="navbar-brand" >
-                           <span>C</span>
-                            <span>H</span>
-                           <span>A</span>
-                           <span>N</span>
-                            <span>D</span>
-                            <span>R</span>
-                             <span>U</span>
-                         </Link>
-               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                       <span class="navbar-toggler-icon"></span>
-               </button>
-               <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                    <Link to="/" class="nav-link active" aria-current="page"  >Home</Link>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <Link to="/" className="navbar-brand">
+            <span>C</span>
+            <span>H</span>
+            <span>A</span>
+            <span>N</span>
+            <span>D</span>
+            <span>R</span>
+            <span>U</span>
+          </Link>
+
+          {/* Navbar toggle button for mobile/tablet view */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          {/* Navbar links */}
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link to="/" className="nav-link active" aria-current="page">
+                  Home
+                </Link>
               </li>
-              <li class="nav-item">
-                <Link to="/About" class="nav-link">About</Link>
+              <li className="nav-item">
+                <Link to="/About" className="nav-link">
+                  About
+                </Link>
               </li>
-              <li class="nav-item">
-                <Link to="/Education" class="nav-link">Education</Link>
+              <li className="nav-item">
+                <Link to="/Education" className="nav-link">
+                  Education
+                </Link>
               </li>
-              <li class="nav-item">
-                <Link to='/Skills' class="nav-link">Skills</Link>
+              <li className="nav-item">
+                <Link to="/Skills" className="nav-link">
+                  Skills
+                </Link>
               </li>
-              <li class="nav-item">
-                <Link class="nav-link" to='/Project'>Project</Link>
+              <li className="nav-item">
+                <Link to="/Project" className="nav-link">
+                  Project
+                </Link>
               </li>
-              <li class="nav-item">
-                <Link to='./Communication' class="nav-link">Contact</Link>
+              <li className="nav-item">
+                <Link to="/Communication" className="nav-link">
+                  Contact
+                </Link>
               </li>
-                </ul>
-            
-                </div>
-               </div>
-              </nav>
-              <Outlet></Outlet>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      
+      {/* Outlet for nested routes */}
+      <Outlet />
     </>
-  )
-}
-export default NavBar
+  );
+};
+
+export default NavBar;
+
+
